8174b41381f79759b162bad919086eda13bb039e
MixelinSoft <mixelinsoft@gmail.com>
2025-01-21 07:58:25 +0200

    add footer component and update layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/UI/Layout/Header/Header';
+import Footer from './components/UI/Layout/Footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
---
+++ src/components/UI/Layout/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import styles from './Footer.module.css';
+
+const Footer = (props) => {
+  return <footer className={styles.footer}>© MixelinSoft 2025</footer>;
+};
+
+export default Footer;
