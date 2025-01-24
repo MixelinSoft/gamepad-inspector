07d7c980c6ee8e6019820479479df5e0b98f6cb0
MixelinSoft <127510809+MixelinSoft@users.noreply.github.com>
2025-01-24 13:53:42 +0200

    remove unused SVG id attribute from XboxOne scheme

--- src/components/GamepadPreview/GamepadScheme/schemes/XboxOne.jsx
+++ src/components/GamepadPreview/GamepadScheme/schemes/XboxOne.jsx
@@ -7,7 +7,6 @@ const XboxOne = (props) => {
       <div>
         <svg
           version='1.1'
-          id='Слой_1'
           xmlns='http://www.w3.org/2000/svg'
           xmlnsXlink='http://www.w3.org/1999/xlink'
           x='0px'
